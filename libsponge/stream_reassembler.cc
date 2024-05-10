9ca85388cf194bd02f9f711cbef0a791c92c3ec1
lihongxu <2872852675@qq.com>
2024-05-10 07:51:50 +0000

    pass the lab1 test

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -32,7 +32,26 @@ StreamReassembler::StreamReassembler(const size_t capacity)
 // 3. aux_storage中存储的字符串不重合且字符串之间一定有间隙，不能将两个相邻的字符串合并。(不能出现字符串a索引为[9, 18], b索引为[19, 23], 因为此时可以将字符串合并为[9, 23])
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
     // 接收到EOF or data为空
-    if (_output.input_ended() || data.empty()) return;
+    if (_output.input_ended()) return;
+    if (data.empty()) {
+        // 下面这段代码是为了解决测试用例中利用空字符串标识结束的情况
+        if (eof && !~_eof_index) {
+            // 很坑！
+            // TODO: 这里和SYN FIN数据为空但是要占一个序列号有关！！
+            // 传空字符串，index=0，eof=1时，是直接结束
+            // 传空字符串，index>0，eof=1时，是在index-1结束
+            if (index)
+                _eof_index = index - 1;
+            else
+                _eof_index = 0;
+        }
+        if (!_eof_index || (static_cast<ssize_t>(_ack) > _eof_index && ~_eof_index)) {
+            _output.end_input();
+            _aux_storage.clear();
+            _unassembler_bytes = 0;
+        }
+        return;
+    }
 
     size_t const st = index;
     size_t const ed = index + data.size() - 1;
@@ -40,6 +59,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         _eof_index = ed;
     }
 
+    // 空字符串不进入下面两个语句
     // case 1: st <= ack and ed >= ack
     // push data to ByteStream and maintain the _aux_storage
     if (st <= _ack && ed >= _ack) {
@@ -53,13 +73,15 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         // 删除重合的data数据
         // 维护 aux_storage, 要求字符串起始idx大于_ack, 且字符串无重合
         string concat_data;
-        size_t concat_len = -1;
-        auto str_iter = lower_bound(_aux_storage.begin(), _aux_storage.end(), make_pair(ed, string()), greater<pair<size_t, string>>());
-        if (str_iter != _aux_storage.end()) {
+        ssize_t concat_len = -1;
+        auto str_iter = _aux_storage.upper_bound(ed + 1);
+        if (str_iter != _aux_storage.begin())
+            str_iter--;
+        if (str_iter != _aux_storage.end() && str_iter->first <= ed + 1) {
             size_t p = str_iter->first, sz = str_iter->second.size();
             if (p + sz > ed + 1) {
                 // "状态约定"第三条保证字符串和字符串之间一定有间隔，所以这里只需要合并一个字符串。
-                concat_data = data + str_iter->second.substr(ed + 1 - p);
+                concat_data = data.substr() + str_iter->second.substr(ed + 1 - p);
                 concat_len = len_w + (sz - ed - 1 + p);
             }
             /* --------------------------------------------------
@@ -69,54 +91,70 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
                除了ByteStream Buffer中的内容，其他所有都要删除此时，_write_data_to_ByteStream会执行clear
             2. ByteStream未能写满，此时删除的这部分已经全部进入到ByteStream的buffer中了，可以执行删除。
             --------------------------------------------------- */
-            _aux_storage.erase(_aux_storage.begin(), str_iter);
+            for (auto iter = _aux_storage.begin(); iter != str_iter; /*no op*/) {
+                _unassembler_bytes -= iter->second.size();
+                iter = _aux_storage.erase(iter);
+            }
+            _unassembler_bytes -= str_iter->second.size();
+            str_iter = _aux_storage.erase(str_iter);
         }
 
         // 写入 ByteStream
-        if (~concat_len) // concat_len == -1
+        if (concat_len > 0)
             _write_data_to_ByteStream(concat_data, concat_data.size() - concat_len, concat_len, index);
         else
             _write_data_to_ByteStream(data, data.size() - len_w, len_w, index);
 
         // 到达EOF
-        if (static_cast<ssize_t>(_ack) > _eof_index) {
+        if (~_eof_index && static_cast<ssize_t>(_ack) > _eof_index) {
             _output.end_input();
             _aux_storage.clear();
+            _unassembler_bytes = 0;
         }
     // case 2: st > _ack
     // just push string to aux_storage and maintain it;
     } else if (st > _ack) {
         // 找到p < st的最后一个字符串
-        auto str_iter = upper_bound(_aux_storage.begin(), _aux_storage.end(), make_pair(st, string()), greater<pair<size_t, string>>());
+        auto str_iter = _aux_storage.lower_bound(st);
+        if (str_iter != _aux_storage.begin()) {
+            str_iter --;
+        }
         // BUG: 有可能找不到p < st的但是能找到下面几种if情况的(符合情况的需要处理)，此时直接将str_iter置为begin
-        if (str_iter == _aux_storage.end()) str_iter = _aux_storage.begin();
+        if (str_iter != _aux_storage.end() && str_iter->first >= st) str_iter = _aux_storage.begin();
 
         string s1, s2;
         if (str_iter != _aux_storage.end()) {
             for (auto iter = str_iter; iter != _aux_storage.end(); /*no op*/) {
                 // p代表当前字符的起始索引，sz代表当前字符串的长度
                 size_t p = iter->first, sz = iter->second.size();
-                if (p >= st && p <= ed + 1 && p + sz > ed + 1) {
-                    // 需要截断，仅会在末端可能出现一次
-                    s2 = std::move(iter->second.substr(ed + 1 - p));
-                    _unassembler_bytes -= sz;
-                    iter = _aux_storage.erase(iter);
-                    break;
-                } else if (p >= st && p + sz <= ed + 1) {
-                    // 需要删除
-                    _unassembler_bytes -= sz;
-                    iter = _aux_storage.erase(iter);
-                } else if (p < st && p + sz >= st) {
-                    // 需要截断，仅会在始端可能出现一次
-                    s1 = std::move(iter->second.substr(0, st - p));
-                    _unassembler_bytes -= sz;
-                    iter = _aux_storage.erase(iter);
-                } else if (p <= st && p + sz >= ed + 1) {
-                    // 数据被包裹住了，直接返回即可
-                    return;
-                }
-                else if (p > ed + 1) {
-                    // 
+
+                if (p < st) {
+                    if (p + sz < st) {
+                        iter ++;
+                        continue;
+                    }
+                    else if (p + sz < ed + 1) {
+                        s1 = std::move(iter->second.substr(0, st - p));
+                        _unassembler_bytes -= sz;
+                        iter = _aux_storage.erase(iter);
+                        continue;
+                    } else {
+                        // 数据被包裹住了，直接返回
+                        return;
+                    }
+                } else if (p >= st && p <= ed + 1) {
+                    if (p + sz <= ed + 1) {
+                        // 被数据包裹住
+                        _unassembler_bytes -= sz;
+                        iter = _aux_storage.erase(iter);
+                        continue;
+                    } else {
+                        s2 = std::move(iter->second.substr(ed + 1 - p));
+                        _unassembler_bytes -= sz;
+                        iter = _aux_storage.erase(iter);
+                        break;
+                    }
+                } else {
                     break;
                 }
             }
@@ -157,6 +195,7 @@ void StreamReassembler::_write_data_to_ByteStream(string const &data, size_t pos
             _aux_storage.erase(iter->first);
         }
     } else {
+        _unassembler_bytes = 0;
         _aux_storage.clear();
     }
 }
